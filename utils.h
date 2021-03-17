f6efd10fed52908a0280de52ca3bc96c70da51c4
Farex-GH <farex.github@gmail.com>
2021-03-16 18:51:17 -0900

    utils: Add assert
    Use this instead of normal assertions
    
    Signed-off-by: Farex-GH <farex.github@gmail.com>

--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <assert.h>
+
 #define ERR_DEBUG 5
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(*x))
@@ -13,4 +15,19 @@
                 } \
 } while (0)
 
+/*
+ * Use instead of normal assertions.
+ * we should catch all asserts before a user sees them, but we don't want the
+ * off-chance that the user runs into an assert and it interrupts the user
+ * experience.
+ *
+ * We have memory to spare, so print the function that would have asserted.
+ */
+#define DBG_ASSERT(x) do {                        \
+    if (ERR_DEBUG) {                              \
+        assert(x);                                \
+    } else {                                      \
+        printf("%s: ASSERT REACHED\n", __func__); \
+    }                                             \
+} while (0)
 #endif
