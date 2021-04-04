b1820130ae594823c94e384b4b13467c9b57f7f0
Farex-GH <farex.github@gmail.com>
2021-04-03 19:56:44 -0900

    pinmap: Add LED pins
    Signed-off-by: Farex-GH <farex.github@gmail.com>

--- pinmap.h
+++ pinmap.h
@@ -6,16 +6,28 @@
 #define BOOSTER_R_D 19
 #define BOOSTER_R_L 18
 #define BOOSTER_R_PRESS 17
+#define BOOSTER_R_LED_1 22
+#define BOOSTER_R_LED_2 26
 
 #define BOOSTER_L_U 6
 #define BOOSTER_L_R 7
 #define BOOSTER_L_D 8
 #define BOOSTER_L_L 9
 #define BOOSTER_L_PRESS 10
+#define BOOSTER_L_LED_1 27
+#define BOOSTER_L_LED_2 28
 
 #define BTN_START 11
+#define BTN_START_LED_1 5
+#define BTN_START_LED_2 4
+
+#define BOOSTER_L_WS2812_DIN 15
+#define BOOSTER_L_NUM_WS2812 10
+#define BOOSTER_R_WS2812_DIN 16
+#define BOOSTER_R_NUM_WS2812 10
 
 #define NUM_BTN 7
 #define NUM_DPAD 4
+#define NUM_WS2812 16
 
 #endif
